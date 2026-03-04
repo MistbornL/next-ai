65577db76bdbd6b44c3a367b78d2dd53d4986962
mistbornl <lashaloria3@gmail.com>
2026-03-04 17:51:51 +0400

    new route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import HeroSection from "@/components/HeroSection";
+import BookCard from "@/components/BookCard";
 
 const Page = () => {
     return (
         <main className="wrapper container">
             <HeroSection />
+
+            <div className="library-books-grid">
+                {books.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+                ))}
+            </div>
         </main>
     )
 }
